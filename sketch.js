860f656c667c160523a2d6a8bc86e9a1930d0837
Aatreyee <70950558+Aatreyee@users.noreply.github.com>
2020-10-22 04:16:27 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -39,7 +39,7 @@ function draw() {
 
 function keyPressed(){
 	if(keyCode===UP_ARROW){ 
-		Matter.Body.applyForce(trash.body,trash.body.position,{x:85,y:-85});
+		Matter.Body.applyForce(trash.body,trash.body.position,{x:55,y:-55});
 		
 	}
 	
